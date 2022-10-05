7941db17372d646de2d9817822cf17eb7c69e9d9
Janelle Forbes <janelle.forbes@gmail.com>
2022-10-04 23:35:28 -0400

    Updated git

---
+++ COMP229-F2022-WEEK2-LAB-301224539/server.js
@@ -0,0 +1,39 @@
+const connect = require('connect');
+
+const app = connect();
+function helloWorld(req, res, next) {
+
+    res.setHeader('Content-Type', 'text/plain');
+ 
+    res.end('Hello from NodeJS Application');
+ 
+ };
+
+ function helloNode(req, res, next) {
+
+    res.setHeader('Content-Type', 'text/html');
+ 
+    res.end('<h1>Hello from NodeJS Application as html<h1>');
+ 
+ };
+
+ function helloJson(req, res, next) {
+
+    res.setHeader('Content-Type', 'application/json');
+ 
+    res.end('Hello from NodeJS Application as json');
+};
+ 
+ 
+ 
+ 
+ app.use('/hello',helloWorld);
+ app.use('/hellonode',helloNode);
+ app.use('/json',helloJson);
+
+app.listen(3000);
+
+
+console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:3000/html');
+console.log('Server running at http://localhost:3000/json');
