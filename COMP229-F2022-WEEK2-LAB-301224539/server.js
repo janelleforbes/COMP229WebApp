3d0b96b88179e99d61fa1d7549d03cd8d994bbde
Janelle Forbes <janelle.forbes@gmail.com>
2022-10-08 21:45:54 -0400

    Components Updated

--- COMP229-F2022-WEEK2-LAB-301224539/server.js
+++ COMP229-F2022-WEEK2-LAB-301224539/server.js
@@ -5,14 +5,21 @@ import logger from 'morgan';
 import session from 'express-session';
 
 import path, {dirname} from 'path';
-import { fileURLToPath } from "url";
+
+import { fileURLToPath } from 'url';
 //get path from application and translate to dirname
-const_dirname = dirname{fileURLToPath(import.meta.url)};
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//Import router
+import indexRouter from '../app/routes/index.route.server.js';
+
+
 
 const app = express();
 
 //setup ViewEnginer EJS
-app.set('views', path.join)(__dirname, '/views');
+app.set('views', path.join)(__dirname, '/app/views');
 app.set('view engine', 'ejs');
 
 app.use(logger('dev')); 
@@ -30,39 +37,12 @@ app.use(session({
 app.use('/', indexRouter);
 
 
-function helloWorld(req, res, next) {
+//linking indexrouter to external application
 
-    res.setHeader('Content-Type', 'text/plain');
- 
-    res.end('Hello from NodeJS Application');
- 
- };
-
- function helloNode(req, res, next) {
-
-    res.setHeader('Content-Type', 'text/html');
- 
-    res.end('<h1>Hello from NodeJS Application as html<h1>');
- 
- };
-
- function helloJson(req, res, next) {
-
-    res.setHeader('Content-Type', 'application/json');
- 
-    res.end('Hello from NodeJS Application as json');
-};
- 
- 
- 
- 
  app.use('/hello',helloWorld);
- app.use('/hellonode',helloNode);
- app.use('/json',helloJson);
+
 
 app.listen(3000);
 
 
 console.log('Server running at http://localhost:3000/');
-console.log('Server running at http://localhost:3000/html');
-console.log('Server running at http://localhost:3000/json');
